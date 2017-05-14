8b79cfaf44d5a21b0b2dd8cec0f2b49a982847bf
David Gaudreault <david.gaudreault@gmail.com>
2017-05-14 15:40:01 -0400

    header, footer & home components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,64 @@
 import { ModuleWithProviders, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { HomeModule } from './home/home.module';
 import {
+  FooterComponent,
+  HeaderComponent,
   SharedModule
 } from './shared';
+
+const rootRouting: ModuleWithProviders = RouterModule.forRoot([], { useHash: true });
 
 @NgModule({
   declarations: [
     AppComponent,
+    FooterComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
+    HomeModule,
+    rootRouting,
     SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
+
+/*import { ModuleWithProviders, NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
+
+import { AppComponent } from './app.component';
+import { HomeModule } from './home/home.module';
+import { HomeComponent } from './home/home.component';
+import { 
+  SharedModule,
+  FooterComponent,
+  HeaderComponent,
+ } from './shared';
+
+const rootRouting: ModuleWithProviders = RouterModule.forRoot([], { useHash: true });
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    FooterComponent,
+    HeaderComponent
+  ],
+  imports: [
+    BrowserModule,
+    SharedModule,
+    HomeModule,
+    rootRouting,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule {}
+*/
